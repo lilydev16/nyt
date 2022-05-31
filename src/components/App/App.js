e69b277eaa77ef920f308a18b0914c42acb96b0c
lswatson16 <lauralynw16@gmail.com>
2022-05-31 18:09:29 -0400

    Set up a fetch request to get articles from the NYT API and store the articles in App's state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,25 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 import Header from '../Header/Header';
 import ArticleList from '../ArticleList/ArticleList';
+import React from 'react';
+import apiCalls from '../../apiCalls';
 
 const App = () => {
+  const [articles, setArticles] = useState([]);
+
+  // const getArticles = () => {
+  //   apiCalls.getTopStories()
+  // }
+  
+  useEffect(() => {
+    apiCalls.getTopStories()
+      .then(data => {
+        console.log(data)
+        setArticles(data.results.slice(0, 10))
+      })
+  }, [])
+
   return (
     <div>
       <Header />
