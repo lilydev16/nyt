29c51a0d97efb5b632e5bf96148b8aa4ffdbe222
lswatson16 <lauralynw16@gmail.com>
2022-05-31 17:31:08 -0400

    Import the Header component to App to display a title

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import Header from '../Header/Header';
 
-function App() {
+const App = () => {
   return (
-    <div className=''>
+    <div>
+      <Header />
       
     </div>
-  );
+  )
 }
 
 export default App;
