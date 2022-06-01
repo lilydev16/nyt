7d7eb76f7e87c8f67da547f457dfa3415451c795
lswatson16 <lauralynw16@gmail.com>
2022-06-01 10:33:03 -0400

    Add error handling

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,7 +3,6 @@ const apiCalls = {
     const apiKey = process.env.REACT_APP_NYT_TECH_CX
     return fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${apiKey}`)
       .then(res => res.json())
-      .then(data => data)
   }
 }
 
