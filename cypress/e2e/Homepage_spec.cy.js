86f0f863cd426f33c77419fe3b9bdd539e2f8805
lswatson16 <lauralynw16@gmail.com>
2022-06-02 18:25:34 -0400

    Implement testing for elements displayed on the UI on page load

--- cypress/e2e/Homepage_spec.cy.js
+++ cypress/e2e/Homepage_spec.cy.js
@@ -18,7 +18,32 @@ describe('Homepage_spec.cy.js', () => {
     cy.url('http://localhost:3000')
   })
 
-  
+  it('should display a header, form, list of articles, and footer', () => {
+    cy.get('header > h1')
+      .should('be.visible').and('contain', 'Top Stories')
+
+    cy.get('footer > h3')
+      .should('be.visible').and('contain', 'Copyright (c) 2022 The New York Times Company. All Rights Reserved.')
+
+    cy.get('form')
+      .should('be.visible')
+      .children('.search-bar').should('be.visible').should('have.attr', 'placeholder').and('include', 'America')
+      
+    cy.get('button.search-btn')
+      .should('be.visible').should('contain', 'Search').and('be.disabled')
+
+    cy.get('.article-list')
+      .should('not.be.empty')
+    
+    cy.get('a')
+      .should('have.length', 15)
+      .children().should('have.class', 'article-card')
+      .within(card => {
+        cy.get('img').should('be.visible').should('have.attr', 'alt').should('not.be.empty')
+        cy.get('h2').should('be.visible').should('not.be.empty')
+        cy.get('p').should('be.visible').should('not.be.empty')
+      })  
+  })
 
 })
 
