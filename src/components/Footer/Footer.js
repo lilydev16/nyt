745b44c40ef9ebc6f6e9c5b78cae423158575f08
lswatson16 <lauralynw16@gmail.com>
2022-08-02 21:32:31 -0400

    Add personal info and social media links to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,11 +2,29 @@ import './Footer.css'
 
 const Footer = () => {
   return(
-    <footer>
-      <h3>
-        Copyright <span>&copy;</span> 2022 The New York Times Company. All Rights Reserved.
-      </h3>
-    </footer>
+    <div className='footer-container'>
+      <footer>
+        <h3>
+          <span>&copy;</span> 
+          2022 The New York Times Company.
+        </h3>
+
+        <div className='personal-container'>
+          <h3 className='personal-info'>
+            Web Development by Lauralyn Watson
+            <div className='personal-links'>
+              <a href='https://github.com/lswatson16/nyt'> 
+                <span>GitHub</span>
+              </a>
+              <a href='https://www.linkedin.com/in/lauralyn-watson/'> 
+                <span>LinkedIn</span>
+              </a>
+            </div> 
+          </h3>
+        </div>
+        
+      </footer>
+    </div>
   )
 }
 
